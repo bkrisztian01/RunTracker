a98321d12767f569349e2c285db2f780e6c584a8
bkrisztian01 <bui.krisztian01@gmail.com>
2022-11-16 18:43:29 +0100

    PermissionDispatcher implementalas

--- RunTracker/app/src/main/java/com/buikr/runtracker/activities/MainActivity.kt
+++ RunTracker/app/src/main/java/com/buikr/runtracker/activities/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.buikr.runtracker.activities
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -14,16 +12,15 @@ import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.buikr.runtracker.R
 import com.buikr.runtracker.databinding.ActivityMainBinding
 import com.buikr.runtracker.fragments.GraphFragment
 import com.buikr.runtracker.fragments.ListFragment
+import permissions.dispatcher.*
 import java.util.*
 
-
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     companion object {
@@ -45,22 +42,12 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .add(binding.fragmentContainer.id, listFragment, "RunList").commit()
         supportFragmentManager.beginTransaction()
-            .add(binding.fragmentContainer.id, graphFragment, "GraphList").hide(graphFragment).commit()
+            .add(binding.fragmentContainer.id, graphFragment, "GraphList").hide(graphFragment)
+            .commit()
 
-        binding.scrollview.setOnScrollChangeListener { v, scrollX, scrollY, oldScrollX, oldScrollY ->
-            run {
-                val dy = oldScrollY - scrollY
-                if (dy < 0 && binding.fabRun.isExtended) {
-                    binding.fabRun.shrink()
-                } else if (dy > 0 && !binding.fabRun.isExtended) {
-                    binding.fabRun.extend()
-                }
-            }
-        }
+        binding.scrollview.setOnScrollChangeListener(::onScrollChange)
 
-        binding.fabRun.setOnClickListener {
-            checkLocationPermission()
-        }
+        binding.fabRun.setOnClickListener { startRunningSessionWithPermissionCheck() }
 
         binding.bottomNavigation.setOnItemSelectedListener(::onBottomNavItemSelected)
 
@@ -68,16 +55,35 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = resources.getColor(R.color.md_theme_light_background)
     }
 
+    private fun onScrollChange(
+        v: View,
+        scrollX: Int,
+        scrollY: Int,
+        oldScrollX: Int,
+        oldScrollY: Int
+    ) {
+        run {
+            val dy = oldScrollY - scrollY
+            if (dy < 0 && binding.fabRun.isExtended) {
+                binding.fabRun.shrink()
+            } else if (dy > 0 && !binding.fabRun.isExtended) {
+                binding.fabRun.extend()
+            }
+        }
+    }
+
     private fun onBottomNavItemSelected(menu: MenuItem): Boolean {
         when (menu.itemId) {
             R.id.runs -> {
-                supportFragmentManager.beginTransaction().hide(activeFragment).show(listFragment).commit();
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(listFragment)
+                    .commit();
                 activeFragment = listFragment
                 binding.fabRun.show()
                 return true
             }
             R.id.graph -> {
-                supportFragmentManager.beginTransaction().hide(activeFragment).show(graphFragment).commit();
+                supportFragmentManager.beginTransaction().hide(activeFragment).show(graphFragment)
+                    .commit();
                 activeFragment = graphFragment
                 binding.fabRun.hide()
                 return true
@@ -86,100 +92,48 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    private fun checkLocationPermission() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(
-                    this,
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            ) {
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-                AlertDialog.Builder(this)
-                    .setTitle(R.string.rationale_dialog_title)
-                    .setMessage(R.string.location_permission_explanation)
-                    .setPositiveButton(
-                        R.string.button_ok
-                    ) { _, _ ->
-                        //Prompt the user once explanation has been shown
-                        requestLocationPermission()
-                    }
-                    .create()
-                    .show()
-            } else {
-                // No explanation needed, we can request the permission.
-                requestLocationPermission()
-            }
-        } else {
-            startRunningSession()
-        }
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun startRunningSession() {
+        startActivity(Intent(this, RunSessionActivity::class.java))
     }
 
-    private fun requestLocationPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
+    @OnPermissionDenied(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun onCallDenied() {
+        Toast.makeText(this, getString(R.string.location_permission_denied), Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    @OnShowRationale(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun showRationaleForLocation(request: PermissionRequest) {
+        val alertDialog = AlertDialog.Builder(this)
+            .setTitle(getString(R.string.location_rationale_title))
+            .setMessage(R.string.location_permission_explanation)
+            .setCancelable(false)
+            .setPositiveButton(R.string.button_ok) { dialog, id -> request.proceed() }
+            .setNegativeButton(R.string.exit) { dialog, id -> request.cancel() }
+            .create()
+        alertDialog.show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun onLocationNeverAskAgain() {
+        Toast.makeText(this, getString(R.string.location_permission_never_ask_again), Toast.LENGTH_LONG)
+            .show()
+        startActivity(
+            Intent(
+                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                Uri.fromParts("package", this.packageName, null),
             ),
-            PERMISSIONS_REQUEST_LOCATION
         )
     }
 
-    @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
         grantResults: IntArray
     ) {
-        when (requestCode) {
-            PERMISSIONS_REQUEST_LOCATION -> {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(
-                            this,
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        ) == PackageManager.PERMISSION_GRANTED
-                    ) {
-                        // Now check background location
-                        startRunningSession()
-                    }
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(this, "Permission denied :(", Toast.LENGTH_LONG).show()
-
-                    // Check if we are in a state where the user has denied the permission and
-                    // selected Don't ask again
-                    if (!ActivityCompat.shouldShowRequestPermissionRationale(
-                            this,
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                        )
-                    ) {
-                        startActivity(
-                            Intent(
-                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
-                                Uri.fromParts("package", this.packageName, null),
-                            ),
-                        )
-                    }
-                }
-                return
-            }
-        }
-    }
-
-    private fun startRunningSession() {
-        startActivity(Intent(this, RunSessionActivity::class.java))
+        onRequestPermissionsResult(requestCode, grantResults)
     }
 }
